d61655ea90e678e52b787a8ac5114fe943947be4
Kiril Saviuk <ksaviuk98@gmail.com>
2023-02-26 17:59:39 +0200

    info

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -7,25 +7,17 @@ const Info = () => {
         <div>
             <div className={style.content}>
                 <div>
-                    <div className={style.contentTextName}>English:</div>
                     <div className={style.contentText}>
                         <strong>This site was developed not primarily to showcase your design skills,
                             but to show your skills in working with the following libraries/frameworks and programming
                             languages: <i>React, Redux, TypeScript, HTML, CSS</i></strong>
                     </div></div>
-                <div>
-                    <div className={style.contentTextName}>Українська:</div>
-                    <div className={style.contentText}>
-                        <strong>Цей сайт був розроблений в основному не для демонстрації своїх дизайнерських навичок,
-                            а щоб показати навички із роботою з такими бібліотеками/фрейворками та мовами
-                            програмування: <i>React, Redux, TypeScript, HTML, CSS</i></strong>
-                    </div></div>
             </div>
             <div className={style.loginization}>         
                 Login and password with which you can test this site:
                 <div className={style.logPass}>
-                <div>Login: <span style={{color: 'red', margin: '50px'}}>welisem178@fsouda.com</span></div>
-                <div>Password: <span style={{color: 'red', margin: '16px'}}>test_profile</span></div>
+                <div>Login: <span style={{color: 'red', margin: '50px'}}>free@samuraijs.com</span></div>
+                <div>Password: <span style={{color: 'red', margin: '16px'}}>free</span></div>
                 </div>
             </div>
             <div className={style.git}>
